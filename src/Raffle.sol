b5cc2d7a90820ce7db3191dbffce6a293587f643
Garba Jamiu <garbajamiu2000@gmail.com>
2024-12-24 10:33:01 +0100

    implement the enterRaffle function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
@@ -10,18 +31,46 @@ pragma solidity 0.8.19;
  */
 
 contract Raffle {
-    uint256 private immutable i_entranceFee;
+    /** Errors */
+    error Raffle__SendMoreToEnterRaffle();
 
-    constructor(uint256 entranceFee) {
+    uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /** Events */
+
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     // Enter the raffle by buying the lottery ticket
     // The amount they buy the ticket will be added to
     // a pool of money which the winner will collect at end.
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough eth sent!"); - Legacy
+        // require(msg.value >= i_entranceFee, Raffle__SendMoreToEnterRaffle()); - This only work on some specific versions.
 
-    function pickWinner() public {}
+        if (msg.value < i_entranceFee) revert Raffle__SendMoreToEnterRaffle();
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
+
+    //TODO:
+    // 1. Get a random number
+    // 2. Use the random number to pick a player
+    // 3. Authomatically call the pickWinner function
+
+    function pickWinner() external {
+        // Check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) revert();
+    }
 
     /** Getter Functions */
 
